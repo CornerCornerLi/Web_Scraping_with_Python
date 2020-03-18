a7c585a1cf5142a78909e4a8135d751af5fce6d2
CornerCornerLi <lzf303@126.com>
2020-03-18 10:17:31 +0800

    Third
    add user_agent&retry when run into 5XX error

--- download.py
+++ download.py
@@ -11,13 +11,19 @@ def tellthetech(url):
 def tellthedomain(url):
     itsdomain = whois.whois(url)
 
-def download(urll):
+def download(url, user_agent='wswp',num_reties=2):
     print('Downloading:%s'%url)
+    headers = {'User-agent': user_agent}
+    request = urllib2.Request(url, headers=headers)
     try:
         html = urllib2.urlopen(url).read()
     except urllib2.URLError as e:
         print('Download error:%s'%e.reason)
         html = None
+        if num_retries > 0:
+            if hasattr(e, 'code') and 500 <= e.code < 600:
+                #retry 5XX HTTP errors
+                return downlaod(url, user_agent, num_retries - 1)
     return html
 
 
